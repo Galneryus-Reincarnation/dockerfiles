5ee3f0e751f13bd7619a4faa8bd698c8bc7dff2e
tsu <tsu@yaroot.net>
2016-07-28 11:44:01 +0800

    remove mirror setting

--- mysql/dist/install.sh
+++ mysql/dist/install.sh
@@ -4,13 +4,6 @@ set -x
 
 cd /opt/dist
 
-mv /etc/apk/repositories /etc/apk/repositories.back
-
-cat > /etc/apk/repositories <<EOF
-http://mirrors.ustc.edu.cn/alpine/v3.3/main/
-http://mirrors.ustc.edu.cn/alpine/v3.3/community/
-EOF
-
 apk update
 apk add mariadb mariadb-client bash s6
 
@@ -27,5 +20,4 @@ EOF
 
 rm -rf /opt/dist
 rm -rf /var/cache/apk/*
-mv /etc/apk/repositories.back /etc/apk/repositories 
 
