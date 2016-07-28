58e0c831419135863d70c963bcd57fbc56ac0517
tsu <tsu@yaroot.net>
2016-07-28 11:42:50 +0800

    .

--- openresty/dist/install.sh
+++ openresty/dist/install.sh
@@ -4,13 +4,6 @@ set -x
 
 cd /opt/dist
 
-mv /etc/apk/repositories /etc/apk/repositories.back
-cat > /etc/apk/repositories <<EOF
-http://mirrors.ustc.edu.cn/alpine/v3.4/main/
-http://mirrors.ustc.edu.cn/alpine/v3.4/community/
-EOF
-
-
 
 apk update
 apk add perl make gcc openssl pcre readline libgcc \
@@ -37,5 +30,4 @@ make install -j4
 apk del perl make gcc openssl-dev pcre-dev musl-dev
 rm -rf /opt/dist
 rm -rf /var/cache/apk/*
-mv /etc/apk/repositories.back /etc/apk/repositories 
 
