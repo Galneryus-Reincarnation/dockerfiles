4ff541fa151065b4365eadc90149e8f4a9fc6b07
tsu <tsu@yaroot.net>
2016-07-28 18:35:54 +0800

    .

--- flywaydb/dist/install.sh
+++ flywaydb/dist/install.sh
@@ -4,8 +4,7 @@ set -x
 
 cd /opt/dist
 
-# TODO switch to https
-wget http://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/4.0.2/flyway-commandline-4.0.2.tar.gz
+wget https://repo.maven.apache.org/maven2/org/flywaydb/flyway-commandline/4.0.2/flyway-commandline-4.0.2.tar.gz
 
 tar zxvf flyway-commandline*
 rm *
