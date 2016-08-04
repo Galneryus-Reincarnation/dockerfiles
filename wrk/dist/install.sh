6fd3eeacb3e682e3e396d0c97a80226a2ae24d7b
tsu <tsu@yaroot.net>
2016-08-04 15:52:39 +0800

    .

--- wrk/dist/install.sh
+++ wrk/dist/install.sh
@@ -5,17 +5,18 @@ set -x
 cd /opt/dist
 
 apk update
-apk add make gcc perl musl-dev openssl curl
+apk add make gcc perl musl-dev openssl
 
 wget https://github.com/wg/wrk/archive/4.0.2.tar.gz
+apk del openssl
 
 tar zxvf *.tar.gz
 cd wrk-*
 
 make -j4
-cp wrk /usr/local/bin
+cp wrk /usr/local/bin/
 
-apk del make gcc perl musl-dev openssl curl
+apk del make gcc perl musl-dev
 
 cd /
 rm -rf /opt/dist
