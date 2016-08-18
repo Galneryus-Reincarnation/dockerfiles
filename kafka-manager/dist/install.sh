26cafad7f7427793516b09ea95ad6ff96c4641f6
tsu <tsu@yaroot.net>
2016-08-18 17:06:04 +0800

    .

--- kafka-manager/dist/install.sh
+++ kafka-manager/dist/install.sh
@@ -17,6 +17,12 @@ unzip target/universal/kafka-manager-*.zip -d /opt
 mv /opt/kafka-manager-* /opt/kafka-manager
 rm -rf /opt/kafka-manager/share
 
+echo 'http.port = ${KM_HTTP_PORT}'            >> /opt/kafka-manager/conf/application.conf
+echo 'kafka-manager.zkhosts= ${KM_ZK_HOSTS}'  >> /opt/kafka-manager/conf/application.conf
+sed -i '/ref="ASYNCFILE"/d'                 /opt/kafka-manager/conf/logback.xml
+sed -i 's/ref="ASYNCSTDOUT"/ref="STDOUT"/'  /opt/kafka-manager/conf/logback.xml
+rm /opt/kafka-manager/conf/logger.xml
+
 cd /
 rm -rf /opt/dist
 rm -rf $HOME/.sbt $HOME/.ivy2
