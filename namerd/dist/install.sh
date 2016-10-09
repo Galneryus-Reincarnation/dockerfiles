1eb2229a6e4907e737665c14541be36136c3a155
tsu <tsu@yaroot.net>
2016-10-09 17:29:36 +0800

    .

--- namerd/dist/install.sh
+++ namerd/dist/install.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-BIN_DIR=/usr/local/bin
-wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.1/namerd-0.8.1-exec -O $BIN_DIR/namerd
-chmod +x $BIN_DIR/namerd
+BIN_FILE=/usr/local/bin/namerd
+wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.1/namerd-0.8.1-exec -O $BIN_FILE
+chmod +x $BIN_FILE
 # TODO add namerctl: https://github.com/BuoyantIO/namerctl
 
 
