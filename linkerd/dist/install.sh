099bf3be6427a3644dbefea977fdeb25ec269ac6
tsu <tsu@yaroot.net>
2016-10-09 17:28:55 +0800

    .

--- linkerd/dist/install.sh
+++ linkerd/dist/install.sh
@@ -3,7 +3,7 @@
 set -x
 
 BIN_FILE=/usr/local/bin/linkerd
-wget https://github.com/BuoyantIO/linkerd/releases/download/0.7.5/linkerd-0.7.5-exec -O $BIN_FILE
+wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.1/linkerd-0.8.1-exec -O $BIN_FILE
 chmod +x $BIN_FILE
 
 cd /
--- namerd/dist/install.sh
+++ namerd/dist/install.sh
@@ -3,7 +3,7 @@
 set -x
 
 BIN_DIR=/usr/local/bin
-wget https://github.com/BuoyantIO/linkerd/releases/download/0.7.5/namerd-0.7.5-exec -O $BIN_DIR/namerd
+wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.1/namerd-0.8.1-exec -O $BIN_DIR/namerd
 chmod +x $BIN_DIR/namerd
 # TODO add namerctl: https://github.com/BuoyantIO/namerctl
 
