8c55efcd3fd731f0218929277ac15d1a22cd5081
tsu <tsu@yaroot.net>
2016-10-10 11:30:08 +0800

    bump ocserv

--- ocserv/dist/install.sh
+++ ocserv/dist/install.sh
@@ -7,7 +7,7 @@ cd /opt/dist
 # sed -i 's#dl-cdn.alpinelinux.org#mirrors.ustc.edu.cn#' /etc/apk/repositories
 # wget http://192.168.3.158:8000/unix/ocserv-0.11.3.tar.xz
 
-wget http://pkgs.fedoraproject.org/repo/pkgs/ocserv/ocserv-0.11.3.tar.xz/ffe3d79662e939bb55dfe7c9e490b4f2/ocserv-0.11.3.tar.xz
+wget http://pkgs.fedoraproject.org/repo/pkgs/ocserv/ocserv-0.11.5.tar.xz/fbda999ce0b528d001bb46b3db6f5d49/ocserv-0.11.5.tar.xz
 # wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.11.3.tar.xz
 
 apk update
