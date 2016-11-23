591aa42bd5cf22cdecaca40f0e3b88ca6fb5ea42
tsu <tsu@yaroot.net>
2016-11-23 12:21:12 +0800

    .

--- linkerd/dist/install.sh
+++ linkerd/dist/install.sh
@@ -6,7 +6,8 @@ BIN_DIR=/usr/local/bin
 
 wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.3/linkerd-0.8.3-exec -O $BIN_DIR/linkerd
 wget https://github.com/BuoyantIO/linkerd/releases/download/0.8.3/namerd-0.8.3-exec -O $BIN_DIR/namerd
-mkdir /opt/linkerd/plugins
+
+mkdir -p /opt/linkerd/plugins
 
 chmod +x $BIN_DIR/*
 
