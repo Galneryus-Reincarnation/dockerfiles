61c4396a1fc514ee585e6d45fc213cf01083821e
tsu <tsu@yaroot.net>
2016-12-08 19:02:09 +0800

    .

---
+++ stress/dist/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+
+cd /opt/dist
+
+apk update
+apk add alpine-sdk
+
+wget http://people.seas.harvard.edu/~apw/stress/stress-1.0.4.tar.gz
+
+tar zxvf *.tar.gz
+cd stress-*/
+
+./configure
+make
+cp src/stress /usr/local/bin/
+
+apk del alpine-sdk
+
+cd /
+rm -rf /opt/dist
+rm -rf /var/cache/apk/*
+
