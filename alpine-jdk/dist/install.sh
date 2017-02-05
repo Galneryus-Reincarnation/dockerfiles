1fc6a9e0842dbd8c263ec708c525da5c358b0d9d
tsu <tsu@yaroot.net>
2017-02-05 14:38:34 +0800

    .

--- alpine-jdk/dist/install.sh
+++ alpine-jdk/dist/install.sh
@@ -4,11 +4,7 @@ set -x
 
 cd /opt/dist
 
-ver=8u121
-build=b13
-
-
-curl -OLH "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/$ver-$build/e9e7ea248e2c4826b92b3f075a80e441/jdk-$ver-linux-x64.tar.gz
+curl -OLH "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz
 curl -OLH "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
 
 JVM_BASE=/usr/jvm
