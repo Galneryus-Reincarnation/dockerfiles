c37ab8c26461da4758e7fb22cff25ae7c613b0c0
tsu <tsu@yaroot.net>
2017-05-09 16:13:30 +0800

    .

--- ocserv/dist/install.sh
+++ ocserv/dist/install.sh
@@ -6,7 +6,7 @@ cd /opt/dist
 
 # sed -i 's#dl-cdn.alpinelinux.org#mirrors.ustc.edu.cn#' /etc/apk/repositories
 
-wget http://pkgs.fedoraproject.org/repo/pkgs/ocserv/ocserv-0.11.7.tar.xz/sha512/a563725bd8753186ece80c91a237a2940071047bb32d1990e2ee122a32ad0468b78a7f35d422dcc9b968a8c56358992842d00958dbf9d743632a03623dd2f5cf/ocserv-0.11.7.tar.xz
+wget http://pkgs.fedoraproject.org/repo/pkgs/ocserv/ocserv-0.11.8.tar.xz/sha512/cea5ef084a15de1e16c0d55f418f454f32c77e4303246f3d11e71ddbc7dbea028282b8200b59a49f5509e786970749b0a9795262639209924bbaa1d619c5c25c/ocserv-0.11.8.tar.xz
 # wget ftp://ftp.infradead.org/pub/ocserv/ocserv-0.11.3.tar.xz
 
 #apk add --no-cache alpine-sdk automake
