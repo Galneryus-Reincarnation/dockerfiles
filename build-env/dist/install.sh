9b21ce36471b19e17bc47131b860b90c19a25bfb
tsu <tsu@yaroot.net>
2017-06-22 20:56:55 +0800

    .

--- build-env/dist/install.sh
+++ build-env/dist/install.sh
@@ -10,7 +10,7 @@ set -x
 # cd /opt/dist
 
 pacman -Syy
-pacman -S --noconfirm jdk8-openjdk python2 base-devel
+pacman -S --noconfirm jdk8-openjdk python2 base-devel wget
 rm -rf /var/cache/pacman/pkg/
 
 rm -rf /opt/dist
