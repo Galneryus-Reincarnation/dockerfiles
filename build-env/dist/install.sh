04ac7bbe6886fbea1cf1cf34b808b35ad6cbd396
tsu <tsu@yaroot.net>
2017-06-30 22:47:10 +0800

    .

--- build-env/dist/install.sh
+++ build-env/dist/install.sh
@@ -7,10 +7,21 @@ echo 'Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/$repo/os/$arch' | t
 
 pacman -Syy
 pacman -S --noconfirm python2 base-devel wget perf cmake
+pacman -S --noconfirm --asdeps ca-certificates-java hicolor-icon-theme java-environment-common java-runtime-common nss xdg-utils
 rm -rf /var/cache/pacman/pkg/
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/jdk.tar.gz -O jdk.tar.gz
-bsdtar jdk.tar.gz && pushd jdk && makepkg -si --noconfirm && popd
+bsdtar xvf jdk.tar.gz
+cd jdk
+chmod og+w .
+
+touch PKGBUILD_
+echo "PKGEXT='.pkg.tar'" >> PKGBUILD_
+cat PKGBUILD >> PKGBUILD_
+mv PKGBUILD_ PKGBUILD
+
+sudo -u nobody makepkg
+pacman -U --noconfirm *.pkg.tar
 
 ## perf agent
 cd /opt
@@ -19,7 +30,7 @@ wget https://github.com/brendangregg/FlameGraph/archive/master.tar.gz -O FlameGr
 
 for x in *.tar.gz;
 do
-    tar zxvf $x
+    bsdtar xvf $x
 done
 
 cd perf-map-agent-master
