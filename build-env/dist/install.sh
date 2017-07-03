0320804b16940fb1ea3dee90f035cc90bac0f709
tsu <tsu@yaroot.net>
2017-07-03 11:26:54 +0800

    .

--- build-env/dist/install.sh
+++ build-env/dist/install.sh
@@ -6,7 +6,7 @@ cd /opt/dist
 echo 'Server = http://ftp.tsukuba.wide.ad.jp/Linux/archlinux/$repo/os/$arch' | tee /etc/pacman.d/mirrorlist
 
 pacman -Syy
-pacman -S --noconfirm python2 base-devel wget perf cmake
+pacman -S --noconfirm python2 base-devel wget perf cmake git
 pacman -S --noconfirm --asdeps ca-certificates-java hicolor-icon-theme java-environment-common java-runtime-common nss xdg-utils
 rm -rf /var/cache/pacman/pkg/
 
