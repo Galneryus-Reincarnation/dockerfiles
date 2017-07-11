1078ac60e3752adb5934db247509964d8fd29479
tsu <tsu@yaroot.net>
2017-07-11 15:42:56 +0800

    .

--- build-env/dist/install.sh
+++ build-env/dist/install.sh
@@ -37,15 +37,19 @@ EOF
 ## perf agent
 cd /opt
 wget https://github.com/jvm-profiling-tools/perf-map-agent/archive/master.tar.gz -O perf-map-agent.tar.gz
-wget https://github.com/brendangregg/FlameGraph/archive/master.tar.gz -O FlameGrap.tar.gz
+wget https://github.com/brendangregg/FlameGraph/archive/master.tar.gz            -O FlameGrap.tar.gz
+wget https://github.com/jvm-profiling-tools/async-profiler/archive/master.tar.gz -O async-profiler.tar.gz
 
 for x in *.tar.gz;
 do
     bsdtar xvf $x
 done
 
-cd perf-map-agent-master
+cd /opt/perf-map-agent-master
 cmake .
+make
+
+cd /opt/async-profiler-master
 make
 
 rm -rf /opt/dist /opt/*.tar.gz
