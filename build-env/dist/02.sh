a027df09339ee32183328c0c6d0436b3ad7f7c16
tsu <tsu@yaroot.net>
2017-07-26 13:11:18 +0800

    .

--- build-env/dist/02.sh
+++ build-env/dist/02.sh
@@ -23,25 +23,25 @@ networkaddress.cache.ttl=30
 networkaddress.cache.negative.ttl=30
 EOF
 
-rm     $JAVA_HOME/*
-rm -rf $JAVA_HOME/lib/visualvm
-rm -rf $JAVA_HOME/lib/missioncontrol
+# rm     $JAVA_HOME/*
+# rm -rf $JAVA_HOME/lib/visualvm
+# rm -rf $JAVA_HOME/lib/missioncontrol
 
-rm     $JAVA_HOME/lib/*javafx*
-rm     $JAVA_HOME/jre/lib/plugin.jar
-rm     $JAVA_HOME/jre/lib/ext/jfxrt.jar
-rm     $JAVA_HOME/jre/bin/javaws
-rm     $JAVA_HOME/jre/lib/javaws.jar
-rm -rf $JAVA_HOME/jre/lib/desktop
-rm -rf $JAVA_HOME/jre/plugin
-rm -rf $JAVA_HOME/jre/lib/deploy*
-rm     $JAVA_HOME/jre/lib/*javafx*
-rm     $JAVA_HOME/jre/lib/*jfx*
-rm     $JAVA_HOME/jre/lib/amd64/libdecora_sse.so
-rm     $JAVA_HOME/jre/lib/amd64/libprism_*.so
-rm     $JAVA_HOME/jre/lib/amd64/libfxplugins.so
-rm     $JAVA_HOME/jre/lib/amd64/libglass.so
-rm     $JAVA_HOME/jre/lib/amd64/libgstreamer-lite.so
-rm     $JAVA_HOME/jre/lib/amd64/libjavafx*.so
-rm     $JAVA_HOME/jre/lib/amd64/libjfx*.so
+# rm     $JAVA_HOME/lib/*javafx*
+# rm     $JAVA_HOME/jre/lib/plugin.jar
+# rm     $JAVA_HOME/jre/lib/ext/jfxrt.jar
+# rm     $JAVA_HOME/jre/bin/javaws
+# rm     $JAVA_HOME/jre/lib/javaws.jar
+# rm -rf $JAVA_HOME/jre/lib/desktop
+# rm -rf $JAVA_HOME/jre/plugin
+# rm -rf $JAVA_HOME/jre/lib/deploy*
+# rm     $JAVA_HOME/jre/lib/*javafx*
+# rm     $JAVA_HOME/jre/lib/*jfx*
+# rm     $JAVA_HOME/jre/lib/amd64/libdecora_sse.so
+# rm     $JAVA_HOME/jre/lib/amd64/libprism_*.so
+# rm     $JAVA_HOME/jre/lib/amd64/libfxplugins.so
+# rm     $JAVA_HOME/jre/lib/amd64/libglass.so
+# rm     $JAVA_HOME/jre/lib/amd64/libgstreamer-lite.so
+# rm     $JAVA_HOME/jre/lib/amd64/libjavafx*.so
+# rm     $JAVA_HOME/jre/lib/amd64/libjfx*.so
 
