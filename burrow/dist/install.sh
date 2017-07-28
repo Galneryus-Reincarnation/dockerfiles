4380f7615a75b5c1baea86ebc72e8722e16603a4
tsu <tsu@yaroot.net>
2017-07-28 12:56:26 +0800

    .

--- burrow/dist/install.sh
+++ burrow/dist/install.sh
@@ -6,12 +6,16 @@ cd /opt/dist
 
 apk add --no-cache git go alpine-sdk
 
+
 git clone https://github.com/linkedin/Burrow
 export GOPATH=/opt/dist/gopath
 export GOROOT=/usr/lib/go
 mkdir $GOPATH
 
 cd Burrow
+wget https://raw.githubusercontent.com/pote/gpm/v1.4.0/bin/gpm
+chmod +x gpm
+./gpm install
 go get
 go build
 mv Burrow /opt
