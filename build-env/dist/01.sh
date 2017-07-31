ce8ffcbe373313e695c53dcd06d43cfc4cee785c
tsu <tsu@yaroot.net>
2017-07-31 13:49:49 +0800

    .

--- build-env/dist/01.sh
+++ build-env/dist/01.sh
@@ -6,5 +6,5 @@ cd /opt/dist
 yum update -y
 yum install epel-release -y
 
-yum install -y which git wget unzip bsdtar gcc gcc-g++ make perf cmake
+yum install -y which git wget unzip bsdtar python-devel gcc gcc-g++ make perf cmake
 
