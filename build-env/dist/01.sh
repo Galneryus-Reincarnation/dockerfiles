f40864bdd9d1a2bc85465633080dd5cb24aa9906
tsu <tsu@yaroot.net>
2017-08-01 16:18:28 +0800

    .

--- build-env/dist/01.sh
+++ build-env/dist/01.sh
@@ -6,7 +6,7 @@ cd /opt/dist
 yum update -y
 yum install epel-release -y
 
-yum install -y which git wget unzip bsdtar \
+yum install -y which git wget unzip bsdtar httpie \
     mariadb postgresql python-devel \
     gcc gcc-g++ make perf cmake
 
