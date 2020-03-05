5fd9da77031235ae7c3bd292e70cbbc3a75ff997
Yan Su <tsu@yaroot.net>
2020-03-05 10:42:47 +0900

    .

--- build-env/dist/04_sbt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -x
-cd /opt/dist
-
-wget https://github.com/paulp/sbt-extras/raw/master/sbt -O /usr/local/bin/sbt
-chmod +x /usr/local/bin/sbt
--- build-env/dist/99.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-# yum erase -y gcc gcc-g++ make perf cmake
-
-rm -rf /opt/dist /opt/*.tar.gz
-
